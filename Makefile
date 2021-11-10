57d539b6249ac843e3b1645fa867e444616fa134
Meercutio <decamerron@gmail.com>
2021-11-10 20:22:05 +0300

    add

---
+++ Makefile
@@ -0,0 +1,35 @@
+NAME	= philo
+CC		= gcc
+FLAGS	= -Wall -Werror -Wextra -g
+
+INC		= -I philosophers.h
+
+SRC_DIR	= src/
+SRC_LST	= main.c utils.c
+
+OBJ_DIR	= obj/
+OBJ_LST	= $(SRC_LST:.c=.o)
+OBJ		= $(addprefix $(OBJ_DIR),$(OBJ_LST))
+
+all:	$(NAME)
+
+$(NAME):	$(OBJ) philosophers.h
+	@$(CC) -o $(NAME) $(OBJ)
+	@echo "##### philosophers compiling finished! #####"
+
+$(OBJ_DIR)%.o:	$(SRC_DIR)%.c
+	@mkdir -p $(OBJ_DIR)
+	@echo "##### Creating" [ $@ ] " #####"
+	@$(CC) $(FLAGS) -o $< -c $< $(INC)
+
+clean:
+	@rm -f $(OBJ)
+	@echo "##### Removed object files #####"
+
+fclean:	clean
+	@rm -f $(NAME)
+	@echo "##### Removed binary files #####"
+
+re:			fclean all
+
+.PHONY:		all clean fclean re
---
+++ philosophers.h
@@ -0,0 +1,40 @@
+#ifndef PHILO_PHILOSOPHERS_H
+# define PHILO_PHILOSOPHERS_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <pthread.h>
+# include <stdlib.h>
+# include <sys/time.h>
+
+struct s_param;
+
+typedef struct s_philoshpher
+{
+	int 			id;
+	int 			meals_count;
+	int 			last_meal;
+	pthread_mutex_t *left;
+	pthread_mutex_t *right;
+	pthread_t 		thread_philo;
+	struct s_param 	*params;
+}		t_philosopher;
+
+typedef struct s_param
+{
+	int 			num_of_philo;
+	int 			time_to_die;
+	int 			time_to_eat;
+	int 			time_to_sleep;
+	int 			num_of_eat;
+	int 			is_dead;
+	pthread_mutex_t *forks;
+	pthread_mutex_t eating;
+	pthread_mutex_t printing;
+	t_philosopher	*philosophers;
+}		t_param;
+
+int	ft_atoi(const char *str);
+int	ft_perror(int err);
+
+#endif
